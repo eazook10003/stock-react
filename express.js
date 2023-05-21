9fb19220a531e721fe9a10c851acf0f8ec912771
Dongwoo Kang <dongwookang@Dongwoos-MacBook-Air.local>
2023-05-20 21:26:04 -0700

    Switch from SQLite to Postgres

--- express.js
+++ express.js
@@ -1,6 +1,103 @@
+// const express = require('express');
+// const cors = require('cors');
+// const sqlite3 = require('sqlite3').verbose();
+// const path = require('path');
+
+// const app = express();
+// app.use(cors());
+
+// // Serve static files from the 'public' directory
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// let db = new sqlite3.Database('./db/schools.db', sqlite3.OPEN_READWRITE, (err) => {
+//     if (err) {
+//     console.error(err.message);
+//   }
+//   console.log('Connected to the schools database.');
+//   defineRoutes();
+// });
+  
+
+// function defineRoutes() {
+//     app.get('/vote', (req, res) => {
+//         const schoolId = Number(req.query.schoolId);
+      
+//         if (isNaN(schoolId)) {
+//           res.status(400).send({message: 'Invalid schoolId'});
+//           return;
+//         }
+      
+//         console.log('Received schoolId: ', schoolId);
+//         let sql = `UPDATE Schools 
+//                    SET likes = likes + 1 
+//                    WHERE id = ?`;
+      
+//         db.run(sql, schoolId, function(err) {
+//           if (err) {
+//             console.error(err.message);
+//             res.status(500).send(err.message);
+//           } else {
+//             console.log(`School with ID: ${schoolId} has received a vote`);
+//             // After updating the likes count in the database, 
+//             // retrieve the updated likes count and send it as a JSON response
+//             db.get(`SELECT likes FROM Schools WHERE id = ?`, [schoolId], (err, row) => {
+//               if (err) {
+//                 console.error(err.message);
+//                 res.status(500).send(err.message);
+//               } else {
+//                 console.log('Received row: ', row);
+//                 res.json({likes: row.likes});
+//               }
+//             });
+//           }
+//         });
+//       });
+      
+
+//   app.get('/schools', (req, res) => {
+//       let sql = 'SELECT * FROM Schools';
+
+//       db.all(sql, [], (err, rows) => {
+//         if (err) {
+//           return console.error(err.message);
+//         }
+//         if (rows) {
+//           res.send(rows);
+//         } else {
+//           res.status(404).send({message: 'No schools found'});
+//         }
+//       });
+//   });
+
+//   app.get('/schools/:id', (req, res) => {
+//     const schoolId = Number(req.params.id);
+  
+//     if (isNaN(schoolId)) {
+//       res.status(400).send({ message: 'Invalid schoolId' });
+//       return;
+//     }
+  
+//     let sql = 'SELECT * FROM Schools WHERE id = ?';
+//     db.get(sql, [schoolId], (err, row) => {
+//       if (err) {
+//         return console.error(err.message);
+//       }
+//       if (row) {
+//         res.send(row);
+//       } else {
+//         res.status(404).send({ message: 'No school found with given ID' });
+//       }
+//     });
+//   });
+  
+
+//   const port = 3000;
+//   app.listen(process.env.PORT || port, () => console.log(`Server listening at http://localhost:${port}`));
+// }
 const express = require('express');
 const cors = require('cors');
-const sqlite3 = require('sqlite3').verbose();
+const { Pool } = require('pg');
 const path = require('path');
 
 const app = express();
@@ -9,88 +106,83 @@ app.use(cors());
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-let db = new sqlite3.Database('./db/schools.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-    console.error(err.message);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
-  console.log('Connected to the schools database.');
-  defineRoutes();
 });
-  
+
+console.log('Connected to the schools database.');
+defineRoutes();
 
 function defineRoutes() {
-    app.get('/vote', (req, res) => {
-        const schoolId = Number(req.query.schoolId);
-      
-        if (isNaN(schoolId)) {
-          res.status(400).send({message: 'Invalid schoolId'});
-          return;
-        }
-      
-        console.log('Received schoolId: ', schoolId);
-        let sql = `UPDATE Schools 
-                   SET likes = likes + 1 
-                   WHERE id = ?`;
-      
-        db.run(sql, schoolId, function(err) {
-          if (err) {
-            console.error(err.message);
-            res.status(500).send(err.message);
-          } else {
-            console.log(`School with ID: ${schoolId} has received a vote`);
-            // After updating the likes count in the database, 
-            // retrieve the updated likes count and send it as a JSON response
-            db.get(`SELECT likes FROM Schools WHERE id = ?`, [schoolId], (err, row) => {
-              if (err) {
-                console.error(err.message);
-                res.status(500).send(err.message);
-              } else {
-                console.log('Received row: ', row);
-                res.json({likes: row.likes});
-              }
-            });
-          }
-        });
-      });
-      
+  app.get('/vote', async (req, res) => {
+    const schoolId = Number(req.query.schoolId);
 
-  app.get('/schools', (req, res) => {
-      let sql = 'SELECT * FROM Schools';
+    if (isNaN(schoolId)) {
+      res.status(400).send({message: 'Invalid schoolId'});
+      return;
+    }
 
-      db.all(sql, [], (err, rows) => {
-        if (err) {
-          return console.error(err.message);
-        }
-        if (rows) {
-          res.send(rows);
-        } else {
-          res.status(404).send({message: 'No schools found'});
-        }
-      });
+    console.log('Received schoolId: ', schoolId);
+    let sql = `UPDATE Schools 
+               SET likes = likes + 1 
+               WHERE id = $1`;
+
+    try {
+      await pool.query(sql, [schoolId]);
+      console.log(`School with ID: ${schoolId} has received a vote`);
+
+      // After updating the likes count in the database, 
+      // retrieve the updated likes count and send it as a JSON response
+      sql = `SELECT likes FROM Schools WHERE id = $1`;
+      const { rows } = await pool.query(sql, [schoolId]);
+      console.log('Received row: ', rows[0]);
+      res.json({likes: rows[0].likes});
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(err.message);
+    }
   });
 
-  app.get('/schools/:id', (req, res) => {
+  app.get('/schools', async (req, res) => {
+    let sql = 'SELECT * FROM Schools';
+
+    try {
+      const { rows } = await pool.query(sql);
+      if (rows.length > 0) {
+        res.send(rows);
+      } else {
+        res.status(404).send({message: 'No schools found'});
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(err.message);
+    }
+  });
+
+  app.get('/schools/:id', async (req, res) => {
     const schoolId = Number(req.params.id);
-  
+
     if (isNaN(schoolId)) {
       res.status(400).send({ message: 'Invalid schoolId' });
       return;
     }
-  
-    let sql = 'SELECT * FROM Schools WHERE id = ?';
-    db.get(sql, [schoolId], (err, row) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      if (row) {
-        res.send(row);
+
+    let sql = 'SELECT * FROM Schools WHERE id = $1';
+    try {
+      const { rows } = await pool.query(sql, [schoolId]);
+      if (rows.length > 0) {
+        res.send(rows[0]);
       } else {
         res.status(404).send({ message: 'No school found with given ID' });
       }
-    });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(err.message);
+    }
   });
-  
 
   const port = 3000;
   app.listen(process.env.PORT || port, () => console.log(`Server listening at http://localhost:${port}`));
